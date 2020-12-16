9dfb6a7d457dd5346ac9a39736ad742104f933cb
Stig B. Dørmænen <stigbd@gmail.com>
2020-12-16 11:53:23 +0100

    chore: rydd opp i lint feil

--- src/sprint_datapusher/datapusher.py
+++ src/sprint_datapusher/datapusher.py
@@ -1,4 +1,4 @@
-"""Module for generate openAPI specifications."""
+"""Module for cli application monitoring directory and send json to webserver."""
 import os
 from typing import Any
 
@@ -6,9 +6,12 @@ import click
 
 from . import __version__
 
+CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 
-@click.command()
+
+@click.command(context_settings=CONTEXT_SETTINGS)
 @click.version_option(version=__version__)
+@click.argument("url", type=click.STRING)
 @click.option(
     "-d",
     "--directory",
@@ -18,10 +21,18 @@ from . import __version__
     type=click.Path(
         exists=True,
         file_okay=False,
-        writable=True,
     ),
 )
-def main(directory: Any) -> None:
-    """Write specification and catalog file based on template for bank."""
+def main(url: str, directory: Any) -> None:
+    """Monitor directory and send content of files as json to webserver URL.
+
+    URL is the url to a webserver exposing an endpoint accepting your json.
+    \f
+    Args:
+        url: the URL to a webserver exposing an endpoint accepting your json.
+        directory: the directory to watch
+    """  # noqa: D301
     # Add a trailing slash to directory if not there:
     directory = os.path.join(directory, "")
+    click.echo(f"Watching directory {directory}")
+    click.echo(f"Sending data to webserver at {url}")
--- tests/test_datapusher.py
+++ tests/test_datapusher.py
@@ -2,6 +2,7 @@
 from click.testing import CliRunner
 import pytest
 from pytest_mock import MockFixture
+
 from sprint_datapusher.datapusher import main
 
 
@@ -11,21 +12,31 @@ def runner() -> CliRunner:
     return CliRunner()
 
 
-def test_main_no_arguments_succeeds(runner: CliRunner) -> None:
+def test_main_with_url_arguments_succeds(runner: CliRunner) -> None:
+    """Should return exit_code 0."""
+    runner = CliRunner()
+
+    result = runner.invoke(main, ["http://example.com"])
+    assert "Watching directory ./" in result.output
+    assert "Sending data to webserver at http://example.com" in result.output
+    assert result.exit_code == 0
+
+
+def test_main_no_arguments_fails(runner: CliRunner) -> None:
     """Should return exit_code 2."""
     runner = CliRunner()
 
     result = runner.invoke(main)
-    assert result.output == ""
-    assert result.exit_code == 0
+    assert "Error: Missing argument 'URL'" in result.output
+    assert result.exit_code == 2
 
 
-def test_main_option_folder_does_not_exist(
+def test_main_option_directory_does_not_exist(
     mocker: MockFixture, runner: CliRunner
 ) -> None:
     """Should return exit_code 0."""
     runner = CliRunner()
 
-    result = runner.invoke(main, ["-d folder"])
+    result = runner.invoke(main, ["-d does_not_exist"])
     assert "does not exist" in result.output
     assert result.exit_code == 2
