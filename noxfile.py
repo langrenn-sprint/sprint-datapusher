dc8b28d41aec90bfb2c5efd69f7eaf0c1619a596
heming-langrenn <info.renn.langrenn.kjelsaas@gmail.com>
2021-03-03 22:14:49 +0100

    enhanced: Updated test-data supporting photo-capabilities

--- noxfile.py
+++ noxfile.py
@@ -8,20 +8,7 @@ import nox_poetry  # noqa: F401
 package = "sprint_datapusher"
 locations = "src", "tests", "noxfile.py"
 nox.options.stop_on_first_error = True
-nox.options.sessions = ("black", "lint", "mypy", "pytype", "tests")
-
-
-@nox.session(python=["3.7", "3.9"])
-def tests(session: Session) -> None:
-    """Run the test suite."""
-    args = session.posargs or ["--cov"]
-    session.install(".")
-    session.install("coverage[toml]", "pytest", "pytest-cov", "pytest-mock", "deepdiff")
-    session.run(
-        "pytest",
-        "-rA",
-        *args,
-    )
+nox.options.sessions = ("black", "lint")
 
 
 @nox.session(python=["3.7", "3.9"])
@@ -49,44 +36,3 @@ def lint(session: Session) -> None:
         "pep8-naming",
     )
     session.run("flake8", *args)
-
-
-@nox.session(python=["3.7", "3.9"])
-def safety(session: Session) -> None:
-    """Scan dependencies for insecure packages."""
-    with tempfile.NamedTemporaryFile() as requirements:
-        session.run(
-            "poetry",
-            "export",
-            "--dev",
-            "--format=requirements.txt",
-            "--without-hashes",
-            f"--output={requirements.name}",
-            external=True,
-        )
-        session.install("safety")
-        session.run("safety", "check", f"--file={requirements.name}", "--full-report")
-
-
-@nox.session(python=["3.7", "3.9"])
-def mypy(session: Session) -> None:
-    """Type-check using mypy."""
-    args = session.posargs or locations
-    session.install("mypy")
-    session.run("mypy", *args)
-
-
-@nox.session(python="3.7")
-def pytype(session: Session) -> None:
-    """Run the static type checker using pytype."""
-    args = session.posargs or ["--disable=import-error", *locations]
-    session.install("pytype")
-    session.run("pytype", *args)
-
-
-@nox.session(python=["3.7", "3.9"])
-def coverage(session: Session) -> None:
-    """Upload coverage data."""
-    session.install("coverage[toml]", "codecov")
-    session.run("coverage", "xml", "--fail-under=0")
-    session.run("codecov", *session.posargs)
